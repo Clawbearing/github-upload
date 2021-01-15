56f8ace00b779bc6af7bf972276438fadfad1906
Ethan <33325974+Clawbearing@users.noreply.github.com>
2021-01-15 09:41:36 -0600

    initial commit - uploading project

---
+++ get input array.ps1
@@ -0,0 +1,18 @@
+﻿$userarray = @()
+
+$inputContinue = $true
+
+While($inputContinue -eq $true){
+    $currentAsset = " "
+    $confirmContinue = "y"
+    $currentAsset = Read-Host "Please enter new hire or leave blank"
+    #adds user to array
+    $userarray += $currentAsset;
+    $confirmContinue = Read-Host "Add another user? y/n"
+    if($confirmContinue -eq "n"){
+        $inputContinue = $false;
+        }
+
+}
+
+Write-Output $userarray
